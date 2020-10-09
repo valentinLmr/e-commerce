e25eb6449678e0870642d8d17f00ed015ccd9574
valentin lemaire <valentinlmr28@gmail.com>
2020-10-09 11:05:46 +0200

    hello

---
+++ frontend/src/App.js
@@ -0,0 +1,58 @@
+import React from "react";
+import logo from "./logo.svg";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import HomeScreen from "./Component/homeScreen";
+import ProductsScreen from "./Component/Products/productsScreen";
+import Product from "./Component/Product/Product";
+import "./App.css";
+
+function App() {
+  const OpensideBar = () =>
+    document.querySelector(".sidebar").classList.add("open");
+  const RemovesideBar = () =>
+    document.querySelector(".sidebar").classList.remove("open");
+
+  return (
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="header">
+          <div className="brand">
+            <button onClick={OpensideBar}> Amazonia</button>
+          </div>
+
+          <div className="header-links">
+            <Link to="/products"> Vêtement</Link>
+            <Link to="products/"> Histoire</Link>
+            <Link to="/products"> Contact</Link>
+            <Link to="/products"> Panier</Link>
+          </div>
+        </header>
+        <aside className="sidebar">
+          <h3>Shopping Categories</h3>
+          <button className="sidebar-close-button" onClick={RemovesideBar}>
+            x
+          </button>
+          <ul>
+            <li>
+              <Link to="/">Pants</Link>
+            </li>
+
+            <li>
+              <Link to="/">Shirts</Link>
+            </li>
+          </ul>
+        </aside>
+        <main className="main">
+          <Route path="/products/:id" exact={true} component={Product} />
+          <Route path="/products" exact={true} component={ProductsScreen} />
+          <Route path="/" exact={true} component={HomeScreen} />
+        </main>
+        <footer className="footer">
+          <p>Thanks for passing By</p>
+        </footer>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ frontend/src/Component/Products/dataProducts.js
@@ -0,0 +1,53 @@
+export default {
+  products: [
+    {
+      id: 1,
+      name: "Bohemian dress",
+      brand: "Banditas",
+      price: "59,99€",
+      image: "../image/OIP.jpeg",
+    },
+    {
+      id: 2,
+      name: "Bohemian dress",
+      brand: "Banditas",
+      price: "59,99€",
+      image: "../image/OIP.jpeg",
+    },
+    {
+      id: 3,
+      name: "Bohemian dress",
+      brand: "Banditas",
+      price: "59,99€",
+      image: "../image/OIP.jpeg",
+    },
+    {
+      id: 4,
+      name: "Bohemian dress",
+      brand: "Banditas",
+      price: "59,99€",
+      image: "../image/OIP.jpeg",
+    },
+    {
+      id: 5,
+      name: "Bohemian dress",
+      brand: "Banditas",
+      price: "59,99€",
+      image: "../image/OIP.jpeg",
+    },
+    {
+      id: 6,
+      name: "Bohemian dress",
+      brand: "Banditas",
+      price: "59,99€",
+      image: "../image/OIP.jpeg",
+    },
+    {
+      id: 7,
+      name: "Bohemian dress",
+      brand: "Banditas",
+      price: "59,99€",
+      image: "../image/OIP.jpeg",
+    },
+  ],
+};
