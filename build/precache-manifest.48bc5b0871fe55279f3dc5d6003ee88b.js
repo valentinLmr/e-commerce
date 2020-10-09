dfd77e83f2cd3ffb166e5a6de8c415c7ec742f58
valentin lemaire <valentinlmr28@gmail.com>
2020-10-09 11:31:50 +0200

    dont

---
+++ build/precache-manifest.48bc5b0871fe55279f3dc5d6003ee88b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c1398e7ea2608902e7fa26ed409a6ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a467f16ec3988f85d21",
+    "url": "/static/css/main.158ab86e.chunk.css"
+  },
+  {
+    "revision": "de8cc29b540cc9ce1366",
+    "url": "/static/js/2.6f48db84.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6f48db84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a467f16ec3988f85d21",
+    "url": "/static/js/main.df37bcf2.chunk.js"
+  },
+  {
+    "revision": "969ec6062bb99b0b9815",
+    "url": "/static/js/runtime-main.a54c6ea9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
